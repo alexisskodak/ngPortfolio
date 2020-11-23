53dfe25ef665880a1d178dae507a15e4d85d57b7
alexis <skodak95alexis@gmail.com>
2020-11-23 16:48:32 +0100

    Fixed inputs, added remove method. Edit form is now disabled after delete

--- src/app/modules/blog/components/info/info.component.ts
+++ src/app/modules/blog/components/info/info.component.ts
@@ -1,47 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {BlogService} from '../../services/blog.service';
 import {PostInterface} from '../../interfaces/post';
 import {NgForm} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class InfoComponent implements OnInit {
+  newPost: PostInterface;
   post: PostInterface;
   postID: number;
   error: string;
+  submitted = false;
 
   constructor(
     private route: ActivatedRoute,
     private blogService: BlogService,
+    private snackbar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     // tslint:disable-next-line:radix
     this.postID = parseInt(this.route.snapshot.paramMap.get('id'));
     setTimeout(() => {
-      this.blogService.getPost(this.postID).subscribe(
-        response => { this.post = response.body; },
-        error => { this.error = error; }
+      this.getPostById(this.postID);
+      }, 1000);
+  }
+
+  getPostById(id: number): void {
+    this.blogService.getPost(this.postID).subscribe(
+      response => { this.post = response.body; },
+      error => { this.error = error; }
       );
-    }, 1000);
+  }
+
+  openSnackBar(msg: string): void {
+    this.snackbar.open(`Post ${msg} successfully`, 'Dismiss', {
+      duration: 2000,
+    });
+  }
+
+  openErrorSnackbar(): void {
+    this.snackbar.open('Title must be at least 5 characters long', '', {
+      duration: 2500,
+      panelClass: 'error-snackbar',
+    });
   }
 
   editPost(f: NgForm): void {
-    this.blogService.updatePost(this.postID, f.value).subscribe(
-      data => {
-        console.log(data);
-        this.post = f.value;
-      }
-    );
+    if (f.valid) {
+      this.blogService.updatePost(this.postID, f.value).subscribe(
+        data => {
+          console.log(data);
+          this.post = f.value;
+        }
+      );
+      this.openSnackBar('edited');
+      this.submitted = true;
+    }
+    else {
+      this.openErrorSnackbar();
+    }
   }
 
-  removePost(): void {
+  removePost(id: number): void {
     // tslint:disable-next-line:radix
-    this.blogService.deletePost(this.postID).subscribe();
+    this.blogService.deletePost(id).subscribe();
+    this.openSnackBar('deleted');
+    this.post.title = '';
+    this.post.body = '';
+    this.submitted = true;
   }
 
 }
--- src/app/modules/blog/components/list/list.component.ts
+++ src/app/modules/blog/components/list/list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Post, PostInterface} from '../../interfaces/post';
 import {BlogService} from '../../services/blog.service';
-import { NgForm } from '@angular/forms';
+import {NgForm} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ListComponent implements OnInit {
-
   // Preparation de la liste des publications
   post = new Post();
   posts: PostInterface[];
@@ -24,6 +25,8 @@ export class ListComponent implements OnInit {
   constructor(
     // Instance du service "BlogService"
     private blogService: BlogService,
+    // Instance du message d'alerte snackbar
+    private snackbar: MatSnackBar,
   ) { }
 
   ngOnInit(): void {
@@ -37,21 +40,29 @@ export class ListComponent implements OnInit {
     );
   }
 
-  pushPost(): void {
-    this.post.id = this.posts.length + 1;
-    this.blogService.addPost(this.post).subscribe(
-      (result) => { this.posts.push(result); }
-    );
+  openSnackBar(): void {
+    this.snackbar.open('Post added successfully', 'Dismiss', {
+      duration: 2000,
+    });
   }
 
-  onTableDataChange(event): void {
-    this.page = event;
-    this.retrievePosts();
+  openErrorSnackbar(): void {
+    this.snackbar.open('Title must be at least 5 characters long', '', {
+      duration: 2500,
+      panelClass: 'error-snackbar',
+    });
   }
 
-  onTableSizeChange(event): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-    this.retrievePosts();
+  pushPost(f: NgForm): void {
+    if (f.valid) {
+      this.blogService.addPost(f.value).subscribe(
+        (result) => { this.posts.push(result); }
+      );
+      this.openSnackBar();
+      f.resetForm();
+    }
+    else {
+      this.openErrorSnackbar();
+    }
   }
 }
